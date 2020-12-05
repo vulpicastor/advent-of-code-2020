0302cdd191c902a60725b325300c2ae4c08af53b
Lizhou Sha <lsha@wisc.edu>
2020-12-05 01:01:43 -0600

    Add comments to Day 5 bash solution

--- src/05.sh
+++ src/05.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# "Part 1 one-liner:"
+# Part 1 one-liner:
 # printf '%d\n' $((2#"$(sed 's/[FL]/0/g;s/[BR]/1/g' < ${1} | sort -n | tail -n 1)"))
 
+# `sed` replaces characters 'F' or 'L' with 0 and 'B' or 'R' with 1, turning
+# seating into binary numbers. `ibase=2` tells the calculator `bc` that input
+# numbers are binary.
 echo "ibase=2; $(sed 's/[FL]/0/g;s/[BR]/1/g' < ${1} | sort -n)" | bc > 05.txt
 MIN=$(head -n 1 05.txt)
 MAX=$(tail -n 1 05.txt)
 echo $MIN $MAX
-seq $MIN $MAX | comm --nocheck-order -23 - 05.dec.txt
+# Because the input file is contiguous except for one number, use `seq` to
+# generate a list of numbers to be compared by `comm`. `comm` complains when
+# the input file is not in lexicographic order, so `--nocheck-order` suppresses
+# the error.
+seq $MIN $MAX | comm --nocheck-order -23 - 05.txt
