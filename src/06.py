c23f9d354b2343005bb2e548f2ac666ea7471708
Lizhou Sha <lsha@wisc.edu>
2020-12-06 14:00:37 -0600

    Linear time solution for Day 6

--- src/06.py
+++ src/06.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
-import re
-
-import numpy as np
-
+import functools
+import operator as op
 
 def main():
     with open("../input/06.txt") as f:
         lines = f.read()
     groups = lines.split('\n\n')
-    all_count = []
+    bits = []
     for g in groups:
         ps = g.split()
-        count = 0
-        for i in "abcdefghijklmnopqrstuvwxyz":
-            if all(i in p for p in ps):
-                count += 1
-        all_count.append(count)
-    groups = [l.replace('\n', '') for l in groups]
-    count = [np.unique(list(i)) for i in groups]
-    print(count)
-    print(sum(map(len, count)))
-    print(all_count)
-    print(sum(all_count))
+        bits.append([
+            sum(
+                # This is essentially a counting sort of the answered questions
+                1 << (ord(c) - ord('a')) for c in p
+            ) for p in ps
+        ])
+    print([[bin(b) for b in a] for a in bits])
+    # Counting if any person answered yes is bitwise or.
+    part1 = (bin(b).count("1") for b in (functools.reduce(op.or_, a) for a in bits))
+    print(sum(part1))
+    # Counting if all persons answered yes is bitwise and.
+    part2 = (bin(b).count("1") for b in (functools.reduce(op.and_, a) for a in bits))
+    print(sum(part2))
 
 if __name__ == "__main__":
     main()
