9801ffefcff5575776e8ec77ed16ca8169d73c31
Lizhou Sha <lsha@wisc.edu>
2020-12-07 00:59:42 -0600

    Self-contained solution for Day 7

--- src/07.py
+++ src/07.py
@@ -1,37 +1,53 @@
 #!/usr/bin/python3
 
+import collections
 import re
+import secrets
 
-import graph
+
+class Node(collections.UserDict):
+
+    def __init__(self):
+        self.data = dict()
+        self.key = secrets.token_bytes(8)
+
+    def __hash__(self):
+        return hash(self.key)
 
 
-DFS_MEMO = dict()
-def dfs_count(node):
-    if node in DFS_MEMO:
-        return DFS_MEMO[node]
+def dfs_visit(node, visited=set()):
+    visited.add(node)
+    for n in node:
+        if n not in visited:
+            dfs_visit(n, visited)
+    return visited
+
+
+def dfs_count(node, visited=dict()):
+    if node in visited:
+        return visited[node]
     if len(node) == 0:
-        DFS_MEMO[node] = 1
+        visited[node] = 1
         return 1
-    counts = sum(d * dfs_count(n) for n, d in node.items()) + 1
-    DFS_MEMO[node] = counts
+    counts = sum(d * dfs_count(n, visited) for n, d in node.items()) + 1
+    visited[node] = counts
     return counts
+
 
 def main():
     with open("../input/07.txt") as f:
         lines = [l.strip().split(' contain ') for l in f.readlines()]
-    edges = []
-    edges2 = []
-    for container, contains in lines:
-        contain_color = container[:-5]
-        contents = re.findall(r'(\d+) (\b.*?\b \b.*?\b) bags?[,.]', contains)
-        for n, c in contents:
-            edges.append((c, contain_color, int(n)))
-            edges2.append((contain_color, c, int(n)))
-    g = graph.Digraph(edges)
-    _, parents = g['shiny gold'].dfs()
-    print(len(parents) - 1)
-    f = graph.Digraph(edges2)
-    print(dfs_count(f['shiny gold']) - 1)
+    graph1 = collections.defaultdict(Node)
+    graph2 = collections.defaultdict(Node)
+    for outer, inners in lines:
+        outer = outer[:-5]
+        inners = re.findall(r'(\d+) (\b.*?\b \b.*?\b) bags?[,.]', inners)
+        for n, c in inners:
+            graph1[c][graph1[outer]] = int(n)
+            graph2[outer][graph2[c]] = int(n)
+    print(len(dfs_visit(graph1['shiny gold'])) - 1)
+    print(dfs_count(graph2['shiny gold']) - 1)
+
 
 if __name__ == "__main__":
     main()
