e5187e98656a399fbd96754c0f5d035c8d049775
Lizhou Sha <lsha@wisc.edu>
2020-12-07 23:18:20 -0600

    Initial solution for Day 8

---
+++ src/08.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+
+def run_code(instructions):
+    pc = 0
+    acc = 0
+    length = len(instructions)
+    visited = np.zeros(length+1, dtype=bool)
+    while not visited[pc]:
+        if pc >= length:
+            break
+        visited[pc] = True
+        opcode, arg = instructions[pc]
+        arg = int(arg)
+        # print(opcode, arg)
+        if opcode == 'acc':
+            acc += arg
+            pc += 1
+            continue
+        elif opcode == 'jmp':
+            pc += arg
+            continue
+        elif opcode == 'nop':
+            pc += 1
+            continue
+    return pc, acc
+
+
+def main():
+    with open("../input/08.txt") as f:
+        lines = [l.strip().split() for l in f.readlines()]
+    length = len(lines)
+    print(run_code(lines))
+    for i, l in enumerate(lines):
+        if l[0] == 'nop':
+            new_code = list(lines)
+            new_code[i] = ('jmp', l[1])
+        if l[0] == 'jmp':
+            new_code = list(lines)
+            new_code[i] = ('nop', l[1])
+        pc, acc = run_code(new_code)
+        if pc == length:
+            print(i, pc, acc)
+            break
+
+
+if __name__ == "__main__":
+    main()
