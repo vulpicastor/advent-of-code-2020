9a834113916992c42967e5b430fce4750ce731c9
Lizhou Sha <lsha@wisc.edu>
2020-12-11 02:04:05 -0600

    Experiment for Day 11
    Part 1 runtime is dramatically reduced, but the alternate solution for
    Part 2 is accidentally longer.

--- src/11.py
+++ src/11.py
@@ -5,6 +5,7 @@ import itertools
 import functools
 import operator as op
 import re
+import timeit
 
 import numpy as np
 
@@ -71,21 +72,147 @@ def step_seat2(seats):
         new_seats.append(new_row)
     return new_seats
 
+def parse_seats(seats):
+    nrows = len(seats)
+    ncols = len(seats[0])
+    seats_array  = np.zeros((nrows, ncols))
+    for i in range(nrows):
+        for j in range(ncols):
+            if seats[i][j] == '#':
+                seats_array[i, j] = 1
+                continue
+            if seats[i][j] == 'L':
+                seats_array[i, j] = 0
+                continue
+            if seats[i][j] == '.':
+                seats_array[i, j] = -1
+                continue
+    return seats_array
+
+def step_seat_fast(seats, out=None):
+    if out is None:
+        count_occupied = np.zeros_like(seats)
+    else:
+        count_occupied = out
+    tmp_seats = seats > 0
+    for left, right in zip(
+        itertools.product([slice(-1), slice(None), slice(1, None)], repeat=2),
+        itertools.product([slice(1, None), slice(None), slice(-1)], repeat=2),
+    ):
+        if left == (slice(None), slice(None)) and right == (slice(None), slice(None)):
+            continue
+        count_occupied[left] += tmp_seats[right]
+    count_occupied[seats>0] = count_occupied[seats>0] < 4
+    count_occupied[seats==0] = count_occupied[seats==0] == 0
+    count_occupied[seats<0] = -1
+    return count_occupied
+
+def gen_check_indices_2(seats):
+    nrows, ncols = seats.shape
+    indices = [[([], []) for _ in range(ncols)] for _ in range(nrows)]
+    with np.nditer(seats, flags=['multi_index']) as it:
+        for x in it:
+            if x < 0:
+                continue
+            i, j = it.multi_index
+            for di, dj in itertools.product(range(-1, 2), repeat=2):
+                if di == 0 and dj == 0:
+                    continue
+                newi = i + di
+                newj = j + dj
+                while 0 <= newi < nrows and 0 <= newj < ncols:
+                    if seats[newi, newj] >= 0:
+                        indices[newi][newj][0].append(i)
+                        indices[newi][newj][1].append(j)
+                        break
+                    newi += di
+                    newj += dj
+    return indices
+
+def step_seat2_fast(seats, check_indices, out=None):
+    if out is None:
+        count_occupied = np.zeros_like(seats)
+    else:
+        count_occupied = out
+    for i, row in enumerate(check_indices):
+        for j, indices in enumerate(row):
+            if indices:
+                count_occupied[i, j] = np.sum(seats[indices])
+    count_occupied[seats>0] = count_occupied[seats>0] < 5
+    count_occupied[seats==0] = count_occupied[seats==0] == 0
+    return count_occupied
+
 
 def iter_seats(seats, step_func):
     old_seats = seats
     new_seats = step_func(seats)
-    while new_seats != old_seats:
+    while np.any(new_seats != old_seats):
         old_seats = new_seats
         new_seats = step_func(old_seats)
     return new_seats
 
+def iter_seats_fast(seats, step_func):
+    old_seats = np.copy(seats)
+    new_seats = step_func(seats)
+    while np.any(new_seats != old_seats):
+        new_seats, old_seats = old_seats, new_seats
+        step_func(old_seats, new_seats)
+    return new_seats
+
+def iter_seats2_fast(seats):
+    check_indices = gen_check_indices_2(seats)
+    # print(check_indices)
+    old_seats = np.copy(seats)
+    new_seats = step_seat2_fast(seats, check_indices)
+    while np.any(new_seats != old_seats):
+        new_seats, old_seats = old_seats, new_seats
+        step_seat2_fast(old_seats, check_indices, new_seats)
+    return new_seats
 
 def main():
     with open("../input/11.txt") as f:
         seats = [list(l.strip()) for l in f.readlines()]
-    print(sum(r.count('#') for r in iter_seats(seats, step_seat)))
-    print(sum(r.count('#') for r in iter_seats(seats, step_seat2)))
+    long_repeat = 1
+    short_repeat = 1
+    print(
+        timeit.timeit(
+            lambda: sum(r.count('#') for r in iter_seats(seats, step_seat)),
+            number=long_repeat,
+        ) / long_repeat,
+        "seconds;",
+        long_repeat,
+        "loops"
+    )
+    parsed_seats = parse_seats(seats)
+    print(
+        timeit.timeit(
+            lambda: np.sum(iter_seats_fast(parsed_seats, step_seat_fast) > 0),
+            number=short_repeat,
+        ) / short_repeat,
+        "seconds;",
+        short_repeat,
+        "loops"
+    )
+
+    print(
+        timeit.timeit(
+            lambda: sum(r.count('#') for r in iter_seats(seats, step_seat2)),
+            number=long_repeat,
+        ) / long_repeat,
+        "seconds;",
+        long_repeat,
+        "loops"
+    )
+
+    print(
+        timeit.timeit(
+            lambda: np.sum(iter_seats2_fast(parsed_seats) > 0),
+            number=short_repeat,
+        ) / short_repeat,
+        "seconds;",
+        short_repeat,
+        "loops"
+    )
     
 
 if __name__ == "__main__":
